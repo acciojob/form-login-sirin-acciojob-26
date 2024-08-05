da2403cf7592595e7e3c1b2fd37da766661559de
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-08-05 16:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+function getFormvalue() {
+    // Get the values of the First and Last name from the form
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    // Display the concatenated First and Last name in an alert
+    alert(firstName + " " + lastName);
+}
 
 
 
